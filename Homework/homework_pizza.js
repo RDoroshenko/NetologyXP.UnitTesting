663dc13645151583c7f3ec6164267f9c66c9aa68
Roman Doroshenko <r.m.doroshenko@gmail.com>
2016-10-17 22:06:33 +0300

    Initial code for testing

--- Homework/homework_pizza.js
+++ Homework/homework_pizza.js
@@ -6,30 +6,55 @@
 // Для оплаты заказа можно использовать средства с бонусного счета
 
 let Pizza = {
-    "Margarita" : {"price": 100},
-    "Pepperoni" : {"price": 250},
-    "HamAndCheese" : {"price": 200},
+    "Margarita" : {"price" : 100},
+    "Pepperoni" : {"price" : 250},
+    "HamAndCheese" : {"price" : 200},
     "Sweet" : {"price" : 0}
+};
+
+let PromoCodes = {
+    "ABCD":   {"discount" : 100}
 };
 
 class Order {
     makePizzaOrder(customer, pizzasOrdered, orderTime, promoCode){
         this.customer = new Customer (customer);
-        this.pizzasOdered = pizzasOrdered;
+        this.pizzasOrdered = pizzasOrdered;
         this.orderTime = orderTime;
         this.promoCode = promoCode;
+        this.addBirthdayPizzaIfBirthday();
         return (true);
     }
     calculateOrderAmount(){
         let result = 0;
-        for (let _pizza in pizzasOrdered) {
-            result += pizzasOrdered[_pizza].quantity * Pizza[_pizza].price;
+        for (let _pizza in this.pizzasOrdered) {
+            result += this.pizzasOrdered[_pizza].quantity * Pizza[_pizza].price;
         }
-       return (result);
+        return (this.makeDayTimeDiscount(result) - this.calculatePromoDiscount(this.promoCode));
     }
     addBirthdayPizzaIfBirthday(){
         if (this.customer.isBirthday()) {pizzasOrdered.Sweet = { quantity : 1 }};
     }
+    calculatePromoDiscount(promoCode){
+        return (typeof PromoCodes[promoCode] === "undefined") ? 0 : PromoCodes[promoCode].discount;
+
+    }
+    checkDayTimeDiscount() {
+        let quantity = 0;
+        for (let _pizza in this.pizzasOrdered) {
+            quantity += this.pizzasOrdered[_pizza].quantity;
+        }
+        return (quantity >= 2 && 10 < this.orderTime.substr(0, 2) && 15 >= this.orderTime.substr(0, 2)) ? true : false;
+    }
+    makeDayTimeDiscount(amount){
+        return this.checkDayTimeDiscount() ? amount * 0.8 : amount;
+    }
+    payWithBonus(){
+        console.log( this.customer.bonusAccount > this.calculateOrderAmount() ? true : false);
+        return this.customer.bonusAccount > this.calculateOrderAmount() ? true : false;
+    }
+
+
 }
 
 class Customer {
@@ -42,6 +67,10 @@ class Customer {
         let _date = new Date();
         return ((_date.getDate() + "." +  (_date.getMonth() + 1)) === this.birthday) ? true : false;
     };
+    addBonus (amount){
+        this.bonusAccount += amount * 0.05;
+        return this.bonusAccount;
+    }
 }
 
 
@@ -54,10 +83,12 @@ let pizzasOrdered = {
 
 let customer = {
     "name": "Ivan",
-    "birthday": "16.10",
-    "bonusAccount" : "500"
+    "birthday": "17.10",
+    "bonusAccount" : 500
 };
 
-order.makePizzaOrder(customer, pizzasOrdered, "16:45");
-order.addBirthdayPizzaIfBirthday();
-console.log("Amount: " + order.calculateOrderAmount() + "\nOrder Time: " + order.orderTime);
+order.makePizzaOrder(customer, pizzasOrdered, "15:45", "ABCD");
+
+
+
+
